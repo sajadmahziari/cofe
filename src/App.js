fdbe83f514cc5dcf20d336369ab689541a6f604a
Sajad Mahziari <mahziarisajad@gmail.com>
2023-07-23 16:56:56 +0330

    remove file extra

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+    <>
+      app test
+    </>
   );
 }
 
