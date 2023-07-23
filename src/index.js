6cb757d3eaccf169d763b184b57541a3b0c65368
Sajad Mahziari <mahziarisajad@gmail.com>
2023-07-23 17:05:16 +0330

    create base poroject

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Layout from './component/share/Layout';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Layout>
+      <App />
+    </Layout>
   </React.StrictMode>
 );
 
