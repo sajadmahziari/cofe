f4cb81ea74d9d0fdb746d1d1ae056d076075478d
sajad <sajad@sajads-MacBook-Pro.local>
2023-08-03 14:26:16 +0330

    add link into categury

--- src/component/categury/Categury.js
+++ src/component/categury/Categury.js
@@ -6,15 +6,16 @@ import pizza from './../../assets/icon/iconCategury/pizza.png'
 import Salad from './../../assets/icon/iconCategury/salad.png'
 import fried from './../../assets/icon/iconCategury/fried.png'
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
 const Categury = ({ closePopUp }) => {
     const [categury] = useState([
-        { name: "نوشیدنی سرد", img: coldDirink },
-        { name: "نوشیدنی گرم", img: hotDirink },
-        { name: "ساندویچ سرد", img: coldSandwitch },
-        { name: "ساندویچ گرم", img: hotSandwitch },
-        { name: "پیتزا", img: pizza },
-        { name: "سالاد", img: Salad },
-        { name: "سوخاری", img: fried },
+        { name: "نوشیدنی سرد", img: coldDirink ,to:"coldDirink"},
+        { name: "نوشیدنی گرم", img: hotDirink ,to:"hotDirink"},
+        { name: "ساندویچ سرد", img: coldSandwitch ,to:"coldSandwitch"},
+        { name: "ساندویچ گرم", img: hotSandwitch ,to:"hotSandwitch"},
+        { name: "پیتزا", img: pizza ,to:"pizza"},
+        { name: "سالاد", img: Salad ,to:"Salad"},
+        { name: "سوخاری", img: fried ,to:"fried"},
     ])
     return (
         <>
@@ -23,10 +24,10 @@ const Categury = ({ closePopUp }) => {
                 {
                     categury.map((cat) => {
                         return (
-                            <div className="itemCateguryParent">
+                            <Link className="itemCateguryParent">
                                 <img className="imgCategury" src={cat.img} />
                                 <span className="nameCategury">{cat.name}</span>
-                            </div>
+                            </Link>
                         )
                     })
                 }
