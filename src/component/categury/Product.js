0c234764ac61f4dc3ca6a625860e0f28d6c38c6b
Sajad Mahziari <mahziarisajad@gmail.com>
2023-11-18 17:51:50 +0330

    design about product

--- src/component/categury/Product.js
+++ src/component/categury/Product.js
@@ -1,13 +1,10 @@
 import { useEffect, useState } from 'react';
 import { AiFillStar } from "react-icons/ai";
 import axios from 'axios';
-
-
 const Product = () => {
     const [product, setProduct] = useState([])
     const [idProduct] = useState(localStorage.getItem('id-product'))
     const regex = /(<([^>]+)>)/gi;
-
     useEffect(() => {
         axios.get(`http://panel.saadcoffee.ir/api/products?token=QSBiJF6Rk%26F5%262yLk1%qz^2*Up&id=${idProduct}`)
             .then((res) => {
@@ -15,13 +12,12 @@ const Product = () => {
             })
             .catch(error => console.log(error));
     }, [])
-
     return (
         <>
             {
-                product.map((pro) => {
+                product.map((pro, index) => {
                     return (
-                        <div className='productOrder'>
+                        <div className='productOrder' key={index}>
                             <img className='productImage' src={`http://panel.saadcoffee.ir/img/products/${pro.image}`} />
                             <div className='d-flex justify-content-between productName  mt-1 w-95 mb-1'>
                                 <span className='f-bold nameProduct'>{pro.title}</span>
@@ -31,12 +27,12 @@ const Product = () => {
                                 </span>
                             </div>
                             <span className='text-right w-95 detaileProduct'> {pro.description.replace(regex, "")} </span>
+                            <div className='w-100 sumPrice mt-1 text-center'>
+                                {/* <span> مجموع:</span> */}
+                                {/* <span>{state.price * counter}</span> */}
+                            </div>
                             <div className=' groupBuy mt-1'>
-                                <div className='productPrice'>
-                                    <span> قیمت: </span>
-                                    <span>{pro.price}</span>
-                                    <span> تومان </span>
-                                </div>
+
                                 {/* <div className='boxOperation flex-row'>
                         <div className='w-30 text-center' onClick={() => setCounter(counter + 1)}>+</div>
                         <div className='w-30 text-center borderRl'>{counter}</div>
@@ -46,9 +42,15 @@ const Product = () => {
 
                                 {/* {state.quantity >= 0 ? "sefr" : 'yek'} */}
                             </div>
-                            <div className='w-100 sumPrice mt-1 text-center'>
-                                {/* <span> مجموع:</span> */}
-                                {/* <span>{state.price * counter}</span> */}
+                            <div className='productPrice'>
+                                <div>
+                                    <span> قیمت: </span>
+                                    <span>{pro.price}</span>
+                                    <span> تومان </span>
+                                </div>
+                                <div className='add-product-white'>
+                                    سفارش
+                                </div>
                             </div>
                         </div>
                     )
