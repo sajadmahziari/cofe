6da81ebf2bce474825d47e2b6b6d8ab722b59c65
Sajad Mahziari <mahziarisajad@gmail.com>
2023-11-18 16:02:47 +0330

    product detailes

--- src/component/categury/Product.js
+++ src/component/categury/Product.js
@@ -1,69 +1,60 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import IceTee from './../../assets/images/drinkCold/IceTee.webp';
-import appleJuse from './../../assets/images/drinkCold/appleJuse.jpg';
-import drinkJuse from './../../assets/images/drinkCold/drinkJuse.webp';
-import iceCofe from './../../assets/images/drinkCold/iceCofe.jpg';
-import iceMoka from './../../assets/images/drinkCold/iceMoka.jpg';
-import mohito from './../../assets/images/drinkCold/mohito.jpg';
-import orangeJuse from './../../assets/images/drinkCold/orangeJuse.jpeg';
-import { BiTrash } from "react-icons/bi";
-import {AiFillStar} from "react-icons/ai";
+import { AiFillStar } from "react-icons/ai";
+import axios from 'axios';
 
 
 const Product = () => {
-    const params = useParams();
-
-    const [product, setProduct] = useState([
-        {quantity: 0, price: 9500, name: "چایی سرد", img: IceTee, id: 1 },
-        {quantity: 0, price: 1000, name: "اب سیب", img: appleJuse, id: 2 },
-        {quantity: 0, price: 1000, name: "آب هندوانه", img: drinkJuse, id: 3 },
-        {quantity: 0, price: 1000, name: "آیس کافه", img: iceCofe, id: 4 },
-        {quantity: 0, price: 1000, name: "آیس موکا", img: iceMoka, id: 5 },
-        {quantity: 0, price: 1000, name: "موهیتو", img: mohito, id: 6 },
-        {quantity: 0, price: 1000, name: "آب پرتقال", img: orangeJuse, id: 7 },
-    ])
-    const idS = params.id;
-    const [state, setState] = useState([]);
+    const [product, setProduct] = useState([])
+    const [idProduct] = useState(localStorage.getItem('id-product'))
+    const regex = /(<([^>]+)>)/gi;
 
     useEffect(() => {
-        const index = product.find((p) => p.id == idS);
-        setState(index)
-    })
-    const [counter, setCounter] = useState(0)
+        axios.get(`http://panel.saadcoffee.ir/api/products?token=QSBiJF6Rk%26F5%262yLk1%qz^2*Up&id=${idProduct}`)
+            .then((res) => {
+                setProduct([res.data])
+            })
+            .catch(error => console.log(error));
+    }, [])
 
     return (
         <>
-            <div className='productOrder'>
-                <img className='productImage' src={state.img} />
-                <div className='d-flex justify-content-between productName  mt-1 w-95 mb-1'>
-                    <span className='f-bold nameProduct'>{state.name}</span>
-                    <span className='w-25 d-flex w-15 justify-content-between align-items-center'>
-                        <span className='starRate'>4</span>
-                        <AiFillStar className='starRate'/>
-                    </span>
-                </div>
-                <span className='text-right w-95 detaileProduct'>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و </span>
-                <div className=' groupBuy mt-1'>
-                    <div className='productPrice'>
-                        <span> قیمت: </span>
-                        <span>{state.price}</span>
-                        <span> تومان </span>
-                    </div>
-                    {/* <div className='boxOperation flex-row'>
+            {
+                product.map((pro) => {
+                    return (
+                        <div className='productOrder'>
+                            <img className='productImage' src={`http://panel.saadcoffee.ir/img/products/${pro.image}`} />
+                            <div className='d-flex justify-content-between productName  mt-1 w-95 mb-1'>
+                                <span className='f-bold nameProduct'>{pro.title}</span>
+                                <span className=' d-flex justify-content-between align-items-center'>
+                                    <span className='starRate pl-1'>4</span>
+                                    <AiFillStar className='starRate' />
+                                </span>
+                            </div>
+                            <span className='text-right w-95 detaileProduct'> {pro.description.replace(regex, "")} </span>
+                            <div className=' groupBuy mt-1'>
+                                <div className='productPrice'>
+                                    <span> قیمت: </span>
+                                    <span>{pro.price}</span>
+                                    <span> تومان </span>
+                                </div>
+                                {/* <div className='boxOperation flex-row'>
                         <div className='w-30 text-center' onClick={() => setCounter(counter + 1)}>+</div>
                         <div className='w-30 text-center borderRl'>{counter}</div>
                         <div className={counter === 0 ? 'w-30 text-center notAction' : 'w-30 text-center'} onClick={() => setCounter(counter - 1)}>{counter > 0 ? "-" : <BiTrash />}</div>
-                    </div> */}
-                    {state.quantity==0&&<div className={`${state.quantity>0?'none':'addProduct ml-1'}`} onClick={() => setCounter(counter + 1)}>{'افزودن'  + state.quantity }  </div>}
-                    
-                    {state.quantity>=0?"sefr":'yek'}
-                </div>
-                <div className='w-100 sumPrice mt-1 text-center'>
-                   <span> مجموع:</span>
-                    <span>{state.price*counter}</span>
-                </div>
-            </div>
+                         </div> */}
+                                {/* {state.quantity == 0 && <div className={`${state.quantity > 0 ? 'none' : 'addProduct ml-1'}`} onClick={() => setCounter(counter + 1)}>{'افزودن' + state.quantity}  </div>} */}
+
+                                {/* {state.quantity >= 0 ? "sefr" : 'yek'} */}
+                            </div>
+                            <div className='w-100 sumPrice mt-1 text-center'>
+                                {/* <span> مجموع:</span> */}
+                                {/* <span>{state.price * counter}</span> */}
+                            </div>
+                        </div>
+                    )
+                })
+            }
+
 
         </>
     );
--- src/component/categury/Products.js
+++ src/component/categury/Products.js
@@ -1,36 +1,21 @@
 import { useEffect, useState } from 'react';
-import IceTee from './../../assets/images/drinkCold/IceTee.webp';
-import appleJuse from './../../assets/images/drinkCold/appleJuse.jpg';
-import drinkJuse from './../../assets/images/drinkCold/drinkJuse.webp';
-import iceCofe from './../../assets/images/drinkCold/iceCofe.jpg';
-import iceMoka from './../../assets/images/drinkCold/iceMoka.jpg';
-import mohito from './../../assets/images/drinkCold/mohito.jpg';
-import orangeJuse from './../../assets/images/drinkCold/orangeJuse.jpeg';
 import { BiTrash } from "react-icons/bi";
 import { Link } from 'react-router-dom';
-import {AiFillStar} from "react-icons/ai";
+import { AiFillStar } from "react-icons/ai";
 import axios from 'axios';
 
 const Products = ({ closePopUp }) => {
-    const [categury, setCategury] = useState([
-        // { showBox: false, quantity: 0, price: 1000, name: "چایی سرد", img: IceTee, id: 1 },
-        // { showBox: false, quantity: 0, price: 1000, name: "اب سیب", img: appleJuse, id: 2 },
-        // { showBox: false, quantity: 0, price: 1000, name: "آب هندوانه", img: drinkJuse, id: 3 },
-        // { showBox: false, quantity: 0, price: 1000, name: "آیس کافه", img: iceCofe, id: 4 },
-        // { showBox: false, quantity: 0, price: 1000, name: "آیس موکا", img: iceMoka, id: 5 },
-        // { showBox: false, quantity: 0, price: 1000, name: "موهیتو", img: mohito, id: 6 },
-        // { showBox: false, quantity: 0, price: 1000, name: "آب پرتقال", img: orangeJuse, id: 7 },
-    ])
-    const [idCatgory]=useState( localStorage.getItem('id-categury'))
-    
-        useEffect(()=>{
-            // setIdCategory(localStorage.getItem('id-categury'))
-            axios.get(`http://panel.saadcoffee.ir/api/products?token=QSBiJF6Rk%26F5%262yLk1%qz^2*Up&category=${idCatgory}`)
+    const [categury, setCategury] = useState([  ])
+    const [idCatgory] = useState(localStorage.getItem('id-categury'))
+
+    useEffect(() => {
+        // setIdCategory(localStorage.getItem('id-categury'))
+        axios.get(`http://panel.saadcoffee.ir/api/products?token=QSBiJF6Rk%26F5%262yLk1%qz^2*Up&category=${idCatgory}`)
             .then((res) => {
                 setCategury(res.data)
             })
             .catch(error => console.log(error));
-        },[])
+    }, [])
 
 
     const addquantity = (id) => {
@@ -71,29 +56,31 @@ const Products = ({ closePopUp }) => {
 
     return (
         <>
-            
+
             <div className='boxCenter'>
-            <p className='groupProducts'>نوشیدنی سرد</p>
+                <p className='groupProducts'>نوشیدنی سرد</p>
 
                 <div className="categury">
                     {
                         categury.map((cat, index) => {
                             return (
-                                <div id={cat.id} key={index} className="itemProduct" onClick={() => console.log('ads')}>
-                                  
+                                <div id={cat.id} key={index} className="itemProduct" onClick={() =>  localStorage.setItem('id-product',cat.id )}>
+
+                                    <Link to={`product/${cat.id}`} className='w-75 d-flex align-items-center  text-decoration-none' onClick={closePopUp}>
                                     <img className="imgProduct w-25" src={`http://panel.saadcoffee.ir/img/products/${cat.image}`} />
-                                    <Link to={`product/${cat.id}`} className='w-50 box-right text-decoration-none' onClick={closePopUp}>
-                                        <span className="nameProduct">{cat.title}</span>
-                                        <span className="PriceProduct"><span>{cat.price} </span><span>تومان</span></span>
-                                        <span className='w-25 d-flex w-15 justify-content-between align-items-center'>
-                                            <span className='starRate'>4</span>
-                                            <AiFillStar className='starRate'/>
-                                        </span>
+                                        <div className='box-right'>
+                                            <span className="nameProduct">{cat.title}</span>
+                                            <span className="PriceProduct"><span>{cat.price} </span><span>تومان</span></span>
+                                            <span className='w-25 d-flex w-15 justify-content-between align-items-center'>
+                                                <span className='starRate'>4</span>
+                                                <AiFillStar className='starRate' />
+                                            </span>
+                                        </div>
                                     </Link>
 
                                     <div className='w-25 box-left'>
                                         {<div className='addItemBasket ml-1' onClick={() => addProduct(cat.id)}>افزودن </div>}
-                                        { <div className='boxOperation d-none'>
+                                        {<div className='boxOperation d-none'>
                                             <div className='text-center addBtn' onClick={() => addquantity(cat.id)}>+</div>
                                             <div className='text-center'>{1}</div>
                                             <div className='text-center minBtn' onClick={() => removeQuantity(cat.id)}>{cat.quantity === 1 ? <BiTrash /> : "-"}</div>
