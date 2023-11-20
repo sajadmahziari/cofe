b1e29b0561b24efc32b2484a64ffbd3a64b682ab
Sajad Mahziari <mahziarisajad@gmail.com>
2023-11-20 16:51:45 +0330

    add api menu list

--- src/component/menu/MenuList.js
+++ src/component/menu/MenuList.js
@@ -3,55 +3,61 @@ import { useEffect, useState } from 'react';
 
 const MenuList = () => {
     const [categures, setCategures] = useState([])
-    const [products, setProducts] = useState([])
     useEffect(() => {
-        axios.get(`http://panel.saadcoffee.ir/api/categories?token=QSBiJF6Rk%26F5%262yLk1%qz^2*Up`)
+        axios.get(`http://panel.saadcoffee.ir/api/menu?token=QSBiJF6Rk%26F5%262yLk1%qz^2*Up`)
             .then((res) => {
                 setCategures(res.data)
             })
             .catch(error => console.log(error));
-        axios.get(`http://panel.saadcoffee.ir/api/products?token=QSBiJF6Rk%26F5%262yLk1%qz^2*Up&category=2`)
-            .then((res) => {
-                setProducts(res.data)
-            })
-            .catch(error => console.log(error));
     }, [])
+
+    const get_product = (cat) => {
+        return (
+            <>
+                {cat.product.map((pros, index) => {
+                    return (
+                        <div className='d-flex flex-wrap justify-content-around align-items-center' key={index}>
+
+                            <div className='products-menu'>
+                                <div className='d-flex justify-content-between align-items-center pl-1'>
+                                    <div className='content-image-product'>
+                                        <img alt="" src={`http://panel.saadcoffee.ir/img/products/${pros.image}`} />
+                                    </div>
+                                    <div className='content-detaile-product'>
+                                        <div className='content-title'>{pros.title}</div>
+                                        <div className='content-price'>{pros.price}</div>
+                                    </div>
+                                    <div className='content-btn-buy'>
+                                        <button>
+                                            خرید
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    )
+                })}
+            </>
+        )
+    }
+
 
 
     return (
         <>
-            {categures.map((cat) => {
+            {categures.map((cat, index) => {
                 return (
-                    <>
-                        <div className='categores-name-menu'>
-                            {cat.title}
+                    <div key={index}>
+                        <div className='categores-name-menu' >
+                            {cat.name}
                         </div>
-                    </>
+                        {get_product(cat)}
+                    </div>
                 )
             })}
 
-            <div className='d-flex flex-wrap justify-content-around align-items-center '>
-                {products.map((pros) => {
-                    return (
-                        <div className='products-menu'>
-                            <div className='d-flex justify-content-between align-items-center pl-1'>
-                                <div className='content-image-product'>
-                                    <img src={`http://panel.saadcoffee.ir/img/products/${pros.image}`} />
-                                </div>
-                                <div className='content-detaile-product'>
-                                    <div className='content-title'>{pros.title}</div>
-                                    <div className='content-price'>{pros.price}</div>
-                                </div>
-                                <div className='content-btn-buy'>
-                                    <button>
-                                        خرید
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    )
-                })}
-            </div>
+
 
 
         </>
